0eb9b22363cf435044ac71137bf09f257609d865
Johnpete8851 <103638903+Johnpete8851@users.noreply.github.com>
2022-04-23 22:42:14 +0530

    Add files via upload

---
+++ caesar_cifer.py
@@ -0,0 +1,43 @@
+alphabet = [
+    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
+    'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd',
+    'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+    't', 'u', 'v', 'w', 'x', 'y', 'z'
+]
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+
+def encrypt(plain_text, shift_amount):
+    cipher_text = ""
+    for letter in plain_text:
+        position = alphabet.index(letter)
+        new_position = position + shift_amount
+        cipher_text += alphabet[new_position]
+    print(f"The encoded text is {cipher_text}")
+
+
+#TODO-1: Create a different function called 'decrypt' that takes the 'text' and 'shift' as inputs.
+def decrypt(letter3, shift_num):
+    orig_text = ""
+    for letter2 in letter3:
+        dec_position = alphabet.index(letter2)
+        new2_position = dec_position - shift_num
+        orig_text += alphabet[new2_position]
+    print(f"orignial text is {orig_text}")
+
+    #TODO-2: Inside the 'decrypt' function, shift each letter of the 'text' *backwards* in the alphabet by the shift amount and print the decrypted text.
+    #e.g.
+    #cipher_text = "mjqqt"
+    #shift = 5
+    #plain_text = "hello"
+    #print output: "The decoded text is hello"
+
+
+#TODO-3: Check if the user wanted to encrypt or decrypt the message by checking the 'direction' variable. Then call the correct function based on that 'drection' variable. You should be able to test the code to encrypt *AND* decrypt a message.
+if direction == "encode":
+    encrypt(plain_text=text, shift_amount=shift)
+if direction == "decode":
+    decrypt(letter3=text, shift_num=shift)
